7eb92336303a4b11568c5f54207a5d7def4d3bff
NegatioN <joakimrishaug@gmail.com>
2016-09-04 16:11:19 +0200

    add encoding

--- vocabulary_program/main.py
+++ vocabulary_program/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 
 word_dict = {'car': 'kuruma'}
@@ -7,11 +10,13 @@ input = os.sys.argv[1]
 def main():
 
     print ('car')
-    if input == 'kuruma':
+    if input == '車':
         print ('correct')
 
     else:
         print ('incorrect')
 
 
+
+main()
     #TODO get input from user
