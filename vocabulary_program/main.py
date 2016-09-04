1ca1ddb5afe166dc0e7c5146fa5532654e9250d2
Kristoffer <Kristoffer>
2016-09-04 16:06:18 +0200

    first draft of vocabulary test

--- vocabulary_program/main.py
+++ vocabulary_program/main.py
@@ -1,2 +1,17 @@
+import os
+
+word_dict = {'car': 'kuruma'}
+
+input = os.sys.argv[1]
 
 def main():
+
+    print ('car')
+    if input == 'kuruma':
+        print ('correct')
+
+    else:
+        print ('incorrect')
+
+
+    #TODO get input from user
