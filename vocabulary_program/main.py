f4806e2b8b5b0f4b6c876c7e2f7c578c4a24cf86
Kristoffer <Kristoffer>
2016-09-04 17:59:26 +0200

    added retry for incorrect input

--- vocabulary_program/main.py
+++ vocabulary_program/main.py
@@ -6,22 +6,23 @@ word_dict = {'car': '車'}
 
 
 def main():
-
     for key_word in word_dict.keys():
         print('What is the Japanese word for: {}'.format(key_word))
-        guessword = input()
+        while True:
+            guessword = input()
 
-        if guessword == word_dict[key_word]:
-            print('Correct! The Japanese word for {} is: {}'.format(key_word, word_dict[key_word]))
-        else:
-            print ('incorrect')
+            if guessword == word_dict[key_word]:
+                print('Correct! The Japanese word for {} is: {}'.format(key_word, word_dict[key_word]))
+                break
 
+            else:
+                print('Incorrect, {} is not the Japanese word for {} '.format(guessword, key_word))
 
 
 main()
 
-#TODO score user
-#TODO add timer
-#TODO continue midway in the test
-#TODO load dictionary
-#TODO if user guessed wrong, try again
+# TODO score user
+# TODO add timer
+# TODO continue midway in the test
+# TODO load dictionary
+# TODO if user guessed wrong, try again
