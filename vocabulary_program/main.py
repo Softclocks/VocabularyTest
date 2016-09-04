a87803743c5df71e7f967602d1af3841700e268e
NegatioN <joakimrishaug@gmail.com>
2016-09-04 16:47:31 +0200

    added formatting for text

--- vocabulary_program/main.py
+++ vocabulary_program/main.py
@@ -8,15 +8,20 @@ word_dict = {'car': '車'}
 def main():
 
     for key_word in word_dict.keys():
-        print ('What is the Japanese word for ' + key_word)
+        print('What is the Japanese word for: {}'.format(key_word))
         guessword = input()
 
         if guessword == word_dict[key_word]:
-            print ('correct')
+            print('Correct! The Japanese word for {} is: {}'.format(key_word, word_dict[key_word]))
         else:
             print ('incorrect')
 
 
 
 main()
-    #TODO get input from user
+
+#TODO score user
+#TODO add timer
+#TODO continue midway in the test
+#TODO load dictionary
+#TODO if user guessed wrong, try again
